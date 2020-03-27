d5efcb2ba317b8faa3e0513cdf0c2e3903337111
IoTLAB <46087841+IoTLAB-IQ@users.noreply.github.com>
2020-03-27 04:48:31 +0300

    Create 6- Flame Sensor.ino

---
+++ 6- Flame Sensor.ino
@@ -0,0 +1,25 @@
+int Buzzer = 4 ;// initializing the pin 9 as the led pin
+int flamepin = 8 ;// initializing pin 12 as the sensor output pin
+
+
+void setup ( ) {
+
+pinMode ( Buzzer , OUTPUT ); // declaring led pin as output pin
+pinMode ( flamepin , INPUT ); // declaring sensor pin as input pin for Arduino
+Serial.begin ( 9600 );// setting baud rate at 9600
+}
+
+void loop ( ) {
+int Read = digitalRead ( flamepin ) ; // reading from the sensor
+if (Read == LOW ) // applying condition
+{
+Serial.println ( " FLAME , FLAME , FLAME " ) ;
+digitalWrite ( Buzzer , HIGH ) ;// if state is high, then turn high the led
+}
+
+else
+{
+Serial.println ( " no flame " ) ;
+digitalWrite ( Buzzer , LOW ) ; // otherwise turn it low
+}
+}
